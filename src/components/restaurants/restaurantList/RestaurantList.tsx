ae18aa23aa868f34c3b03c0a9c110b2a22226c3e
Andy Tisdall <andrew.tisdall@gmail.com>
2024-08-01 19:13:25 -0700

    fix gesture responder bugs on older iphones

--- src/components/restaurants/restaurantList/RestaurantList.tsx
+++ src/components/restaurants/restaurantList/RestaurantList.tsx
@@ -8,7 +8,7 @@ import baseStyles from '../../styles/baseStyles';
 
 import Btn from '../../reusable/Btn';
 
-// const ZOOM_THRESHOLD = 150;
+const ZOOM_THRESHOLD = 150;
 
 const RestaurantList = ({
   restaurants,
@@ -19,54 +19,55 @@ const RestaurantList = ({
   onRestaurantPress: (id: string) => void;
   resetFilterState: () => void;
 }) => {
-  const [zoom] = useState(2);
-  // const [, setStartDist] = useState(0);
+  const [zoom, setZoom] = useState(2);
+  const [, setStartDist] = useState(0);
 
-  // const calcDistance = useCallback((coords1: number[], coords2: number[]) => {
-  //   let dx = Math.abs(coords1[0] - coords2[0]);
-  //   let dy = Math.abs(coords1[1] - coords2[1]);
-  //   return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-  // }, []);
+  const calcDistance = useCallback((coords1: number[], coords2: number[]) => {
+    let dx = Math.abs(coords1[0] - coords2[0]);
+    let dy = Math.abs(coords1[1] - coords2[1]);
+    return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+  }, []);
 
-  // const panResponder = useRef(
-  //   PanResponder.create({
-  //     onStartShouldSetPanResponder: () => true,
-  //     onMoveShouldSetPanResponder: () => true,
-  //     onPanResponderStart: event => {
-  //       const touches = event.nativeEvent.touches;
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: (e, gesture) =>
+        gesture.dx !== 0 && gesture.dy !== 0,
+      onPanResponderStart: event => {
+        const touches = event.nativeEvent.touches;
 
-  //       if (touches.length >= 2) {
-  //         setStartDist(
-  //           calcDistance(
-  //             [touches[0].pageX, touches[0].pageY],
-  //             [touches[1].pageX, touches[1].pageY],
-  //           ),
-  //         );
-  //       }
-  //     },
-  //     onPanResponderMove: event => {
-  //       const touches = event.nativeEvent.touches;
+        if (touches.length >= 2) {
+          setStartDist(
+            calcDistance(
+              [touches[0].pageX, touches[0].pageY],
+              [touches[1].pageX, touches[1].pageY],
+            ),
+          );
+        }
+      },
+      onPanResponderMove: event => {
+        const touches = event.nativeEvent.touches;
 
-  //       setStartDist(currentDist => {
-  //         if (touches.length >= 2) {
-  //           const distance = calcDistance(
-  //             [touches[0].pageX, touches[0].pageY],
-  //             [touches[1].pageX, touches[1].pageY],
-  //           );
-  //           if (distance - currentDist < -ZOOM_THRESHOLD) {
-  //             setZoom(current => (current < 3 ? current + 1 : current));
-  //             return distance;
-  //           }
-  //           if (distance - currentDist > ZOOM_THRESHOLD) {
-  //             setZoom(current => (current > 1 ? current - 1 : current));
-  //             return distance;
-  //           }
-  //         }
-  //         return currentDist;
-  //       });
-  //     },
-  //   }),
-  // ).current;
+        setStartDist(currentDist => {
+          if (touches.length >= 2) {
+            const distance = calcDistance(
+              [touches[0].pageX, touches[0].pageY],
+              [touches[1].pageX, touches[1].pageY],
+            );
+            if (distance - currentDist < -ZOOM_THRESHOLD) {
+              setZoom(current => (current < 3 ? current + 1 : current));
+              return distance;
+            }
+            if (distance - currentDist > ZOOM_THRESHOLD) {
+              setZoom(current => (current > 1 ? current - 1 : current));
+              return distance;
+            }
+          }
+          return currentDist;
+        });
+      },
+    }),
+  ).current;
 
   const renderItem = useCallback(
     ({item}: {item: Restaurant}) => (
@@ -109,7 +110,7 @@ const RestaurantList = ({
   if (restaurants) {
     return (
       <MasonryFlashList
-        // {...panResponder.panHandlers}
+        {...panResponder.panHandlers}
         estimatedItemSize={getZoomHeight()}
         numColumns={zoom}
         data={restaurants}
